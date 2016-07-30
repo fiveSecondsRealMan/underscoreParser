189b9b9b16dec193e5fea69d16b7e2c2f19bb4a2
wjj <13524220689@189.cn>
2016-07-31 00:48:18 +0800

    second commit

--- underscoreParser.js
+++ underscoreParser.js
@@ -53,7 +53,9 @@
 
     // 优化回调函数，
     // 根据argCount返回适合各种业务带有详细参数的函数并且内部改变func的上下文的调用
-    // 如果argCount为1，function (value) { func.call(context, value) }，常用于对数组的遍历
+    // 如果argCount为1，function (value) { func.call(context, value) }，常用于对数组的值的遍历
+    // 如果argCount为2，function (value, other) { func.call(context, value, other) }，常用于对数组的reduce
+    // 如果argCount为3，function (value, index, collection) { func.call(context, value, index, collection) }，常用于对数组的值，索引的遍历
     // 考察的是apply、call、void 0
     var optimizeCb = function (func, context, argCount) {
         if (context === void 0)
@@ -68,11 +70,87 @@
                 return func.call(context, value, other);
             };
 
-            
+            case 3: return function (value, index, collection) {
+                return func.call(context, value, index, collection);
+            };
 
+            case 4: return function (accumulator, value, index, collection) {
+                return func.call(context, accumulator, value, index, collection);
+            };
         }
 
+        return function () {
+            return func.apply(context, arguments);
+        };
+    };
 
+    // 如果value为null、undefined，返回对数组值的遍历的函数
+    // 如果value为Function类型，返回对value的优化函数
+    // 如果value为Object类型，返回匹配value中的键值对的函数
+    // 如果不满足以上几种情况，则返回一个获取对象以value作为key的值的函数
+    var cb = function (value, context, argCount) {
+        if (value == null)
+            return _.identity;
+
+        if (_.isFunction(value))
+            return optimizeCb(value, context, argCount);
+
+        if (_.isObject(value))
+            return _.matcher(value);
+
+        return _.property(value);
+    };
+
+    _.iteratee = function(value, context) {
+        return cb(value, context, Infinity);
+    };
+
+    // 创建属性分配器函数
+    var createAssigner = function (keysFunc, undefinedOnly) {
+        return function(obj) {
+            var length = arguments.length;
+
+            if (length < 2 || obj == null)
+                return obj;
+
+            for (var index = 1; index < length; index++) {
+                var source = arguments[index],
+                    keys = keysFunc(source),
+                    l = keys.length;
+                for (var i = 0; i < l; i++) {
+                    var key = keys[i];
+                    if (!undefinedOnly || obj[key] === void 0) obj[key] = source[key];
+                }
+            }
+
+            return obj;
+        };
+    };
+
+    // 创建一个继承prototype的新对象的函数
+    // 如果浏览器环境、nodejs环境支持es5，则使用es5的Object.create方法
+    // 理解js的prototype继承
+    var baseCreate = function (prototype) {
+        if (!_.isObject(prototype))
+            return {};
+
+        if (nativeCreate)
+            return nativeCreate(prototype);
+
+        // 将Ctor.prototype指向prototype
+        Ctor.prototype = prototype;
+        // 实例化Ctor时，根据js new操作符的定义，将产生通过原型继承的新对象result，因为js的原型继承基于原型链
+        // 还需要理解js中的引用类型
+        var result = new Ctor;
+        Ctor.prototype = null;
+        return result;
+    };
+
+    // 返回key在对象中对应的值
+    var property = function(key) {
+        return function(obj) {
+            return obj == null ? void 0 : obj[key];
+        };
     };
 
 })();
